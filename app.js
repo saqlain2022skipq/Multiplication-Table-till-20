8bb3448b6c07d641b35366977e18e4ea7e237ad7
H.M. Saqlain Mushtaque <120879519+saqlain2022skipq@users.noreply.github.com>
2024-01-25 20:06:43 +0500

    Update app.js

--- app.js
+++ app.js
@@ -4,12 +4,11 @@ confirm (" Do you want to see the Table of any Number?")
 
 var num = prompt("Enter the number: ")
 
-document.write(`TABLE OF ${num}`+"<br>")
-
 if (num <= 20){
 
     for (num; num<=20; num++){
-
+        
+        document.write("<br>"+`TABLE OF ${num}`+"<br>")
         for (i=1 ; i<= 10; i++){
             document.write(`${num} x ${i} = `, num*i + "<br>");
         }
